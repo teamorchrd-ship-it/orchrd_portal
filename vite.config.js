663bf7d4e4533270602b1f06262b14bfbbe8f796
Fardeen Khan <74563996+fardeen85@users.noreply.github.com>
2026-06-23 01:52:13 +0500

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: "/orchrd_portal/", // GitHub Pages repo subpath
-  plugins: [react()],
-})
