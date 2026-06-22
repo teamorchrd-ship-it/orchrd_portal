ce92517ea636d16b979452f4a3d07592dc7f463b
Fardeen Khan <74563996+fardeen85@users.noreply.github.com>
2026-06-22 21:07:39 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/", // 🔴 your repo name
+  base: "/orchrd_portal/", // GitHub Pages repo subpath
   plugins: [react()],
 })
