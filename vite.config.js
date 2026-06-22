33526ff0a1307dc0d4459e64fdce7d655c940bef
Fardeen Khan <74563996+fardeen85@users.noreply.github.com>
2026-06-22 20:54:41 +0500

    updated path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/orchrd_portal/", // 🔴 your repo name
+  base: "/", // 🔴 your repo name
   plugins: [react()],
 })
